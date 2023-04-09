2f6766f4ec1a9056c5143ed88579013a4ce0e970
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-04-10 01:09:33 +0530

    commit

--- GSI.py
+++ GSI.py
@@ -1,12 +1,13 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+import math
 
 # Adjustment for hands
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
-    min_detection_confidence=0.5,
+    min_detection_confidence=0.3,  # lower detection confidence threshold
     min_tracking_confidence=0.9,
     max_num_hands=1
 )
@@ -19,51 +20,54 @@ target_fps = 60
 #  delay to  FPS
 delay = int(1000 / target_fps)
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # wider area coverage
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-
-
-#main loop
+# main loop
 while True:
-    #frame from webcam
+    # frame from webcam
     ret, frame = cap.read()
     if not ret:
         break
-    
-    # Flippung image
+
+    # Flipping image
     frame = cv2.flip(frame, 1)
-    
+
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
     if results.multi_hand_landmarks:
-       
+
         hand_landmarks = results.multi_hand_landmarks[0]
-        
+
         mp_drawing.draw_landmarks(
             frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
- 
+
         index_pos = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
         thumb_pos = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-        
+
         index_x, index_y = int(index_pos.x * screen_width), int(index_pos.y * screen_height)
         thumb_x, thumb_y = int(thumb_pos.x * screen_width), int(thumb_pos.y * screen_height)
-        
-        # Handle click gesture
-        if abs(index_y - thumb_y) < 40:
+
+        # Calculate Euclidean distance between index finger and thumb tip landmarks
+        distance = math.sqrt((index_x - thumb_x)**2 + (index_y - thumb_y)**2)
+
+        # Check if distance is below a threshold
+        if distance < 70:
             pyautogui.click()
             pyautogui.sleep(0.1)
         else:
             if prev_index_x is None:
                 prev_index_x, prev_index_y = index_x, index_y
             else:
-                factor = 0.5 
+                factor = 0.5
                 new_x = (1 - factor) * prev_index_x + factor * index_x
                 new_y = (1 - factor) * prev_index_y + factor * index_y
                 pyautogui.moveTo(new_x, new_y)
                 prev_index_x, prev_index_y = new_x, new_y
-        
+
     # Display frame
     cv2.imshow('Virtual Mouse', frame)
-    
+
     # Exit on 'q' key press
     if cv2.waitKey(delay) & 0xFF == ord('q'):
         break
